549f6ea0348cfd7ecd370233b396841e750baf94
Elior Erez <elior123@gmail.com>
2022-08-05 00:28:31 +0300

    Add base code and set project structure

---
+++ src/nutanix_api/__init__.py
@@ -0,0 +1,15 @@
+from .api_client import NutanixApiClient, NutanixSession
+from .virtual_machine import PowerState, VirtualMachine, VMSpec, VMMetadata, VMStatus
+
+
+__all__ = [
+    "VMSpec",
+    "PowerState",
+    "VirtualMachine",
+    "NutanixApiClient",
+    "NutanixSession",
+    "PowerState",
+    "VMMetadata",
+    "VMStatus",
+    "exceptions"
+]
---
+++ src/nutanix_api/exceptions.py
@@ -0,0 +1,7 @@
+
+class RequestError(Exception):
+    pass
+
+
+class NutanixAPIError(RequestError):
+    pass
